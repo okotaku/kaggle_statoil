ec52a57e1a538dd0777f9598077b7a967497595c
okotaku <to78314910@gmail.com>
2017-12-28 10:01:13 +0900

    update vgg16.py

--- model/vgg16.py
+++ model/vgg16.py
@@ -4,7 +4,7 @@ from keras.layers.core import Activation
 from keras.models import Sequential, Model
 
 
-def Vgg16():
+def Vgg16(freeze_leyer):
     input_tensor = Input(shape=(75, 75, 3))
     vgg16 = VGG16(include_top=False, weights='imagenet',
                   input_tensor=input_tensor)
@@ -20,7 +20,7 @@ def Vgg16():
     top_model.add(Dense(1, activation='sigmoid'))
 
     model = Model(input=vgg16.input, output=top_model(vgg16.output))
-    for layer in model.layers[:13]:
+    for layer in model.layers[:freeze_leyer]:
         layer.trainable = False
 
     return model
